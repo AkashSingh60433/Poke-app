d0766d74ada33ae6b923083fd6713972357a21c1
Akashsingh60433 <akashsingh6043@gmail.com>
2024-09-29 17:52:20 -0500

    Exercise1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
-let pokemonList = [];
+let pokemonRepository = (function () {
 
-pokemonList.push({
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"]
-});
-
-pokemonList.push({
-    name: "Charmander",
-    height: 6,
-    types: ["fire"]
-});
-
-pokemonList.push({
-    name: "Squirtle",
-    height: 5,
-    types: ["water"]
-});
-
-for (let i = 0; i < pokemonList.length; i++) {
-    let output = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
-
-    if (pokemonList[i].height > 6) {
-        output += " - Wow, that’s big!";
+    let pokemonList = [
+      { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+      { name: 'Charmander', height: 0.6, types: ['fire'] },
+      { name: 'Squirtle', height: 0.5, types: ['water'] }
+    ];
+  
+    // Function to add a new Pokémon to the pokemonList
+    function add(pokemon) {
+      pokemonList.push(pokemon);
     }
-
-    document.write(output + "<br>");
-}
+  
+    // Function to return all Pokémon in the list
+    function getAll() {
+      return pokemonList;
+    }
+  
+    // Return an object that exposes the public functions
+    return {
+      add: add,
+      getAll: getAll
+    };
+})();
+  
+  // Adding a new Pokémon to the repository using the add function
+  pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
+  
+  // Retrieve all Pokémon and display each one using forEach
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    console.log(`Name: ${pokemon.name}, Height: ${pokemon.height}, Types: ${pokemon.types.join(', ')}`);
+});
+  
